e14e92fcb844e3a42202ec3bf858d0d4abf9e861
tofishes <caiceclb@163.com>
2013-08-06 19:00:43 +0800

    增加asyncDo异步解决方案。

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var express = require('express')
-,   init = require('./init');
+,   init = require('./init')
+,   asyncDo = require('./libs/asyncDo')
+,   db = require('mongoskin').db('localhost:27017/umitime');
 
 var app = express();
 // var package = require('./package.json');
@@ -28,7 +30,38 @@ app.use('/', function(req, res, next){
 init(app);
 
 app.get('/aa', function( req, res){
-    res.send('hello world');
+    var bands = [], todos = asyncDo.todos([1, 2, 3]);
+    db.collection('bands').find({year: 1984}).toArray(function(err, result) {
+        var bands = [];
+        for (var i = 0; i < result.length; i++) {
+            bands.push('<p>band name: ', result[i].name ,' @', result[i].year ,'</p>');
+        }
+        todos.done(1, {
+            'bands': bands.join('')
+        });
+    });
+    db.collection('bands').find({year: 1985}).toArray(function(err, result) {
+        var bands = [];
+        for (var i = 0; i < result.length; i++) {
+            bands.push('<p>band name: ', result[i].name ,' @', result[i].year ,'</p>');
+        }
+        todos.done(2, {
+            'bands2': bands.join('')
+        });
+    });
+    db.collection('bands').find({year: 2002}).toArray(function(err, result) {
+        var bands = [];
+        for (var i = 0; i < result.length; i++) {
+            bands.push('<p>band name: ', result[i].name ,' @', result[i].year ,'</p>');
+        }
+        todos.done(3, {
+            'bands3': bands.join('')
+        });
+    });
+
+    todos.allDone(function (dones) {
+        res.send('hello world!...<a href="javascript:self.close()" >关闭窗口</a>' + dones.bands + dones.bands2 + dones.bands3);
+    });
 });
 
 app.use(function(err, req, res, next){
