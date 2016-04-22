bd894f7c905ee67531baa2af8ce922a096ba7061
Fish 常小波 <changxiaobo@gdsiyue.com>
2016-04-22 10:45:28 +0800

    update add postgresql test
    Signed-off-by: Fish 常小波 <changxiaobo@gdsiyue.com>

---
+++ pg-test.js
@@ -0,0 +1,31 @@
+var pg = require('pg');
+var conString = "postgres://postgres:tofishes@localhost/test";
+
+//this initializes a connection pool
+//it will keep idle connections open for a (configurable) 30 seconds
+//and set a limit of 10 (also configurable)
+pg.connect(conString, function(err, client, done) {
+  if(err) {
+    return console.error('error fetching client from pool', err);
+  }
+  client.query('SELECT $1::int AS number', ['1'], function(err, result) {
+    //call `done()` to release the client back to the pool
+    done();
+
+    if(err) {
+      return console.error('error running query', err);
+    }
+    console.log(result.rows[0].number);
+    //output: 1
+  });
+  client.query('SELECT * from job', function(err, result) {
+    //call `done()` to release the client back to the pool
+    done();
+
+    if(err) {
+      return console.error('error running query', err);
+    }
+    console.log(result.rows.length);
+    //output: 1
+  });
+});
